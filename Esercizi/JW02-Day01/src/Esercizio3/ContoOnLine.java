58abb0fc37b69e1070450650eba779d93efd86b9
promi93 <116350522+promi93@users.noreply.github.com>
2023-05-08 18:08:53 +0200

    l...

---
+++ Esercizi/JW02-Day01/src/Esercizio3/ContoOnLine.java
@@ -0,0 +1,22 @@
+package Esercizio3;
+
+public class ContoOnLine extends ContoCorrente {
+    double maxPrelievo;
+
+    ContoOnLine(String titolare, double saldo, double maxP) {
+        super(titolare, saldo);
+        this.maxPrelievo = maxP;
+    }
+
+    void stampaSaldo() {
+        System.out.println("Titolare: " + titolare + " - Saldo: " + saldo + " - Num movimenti: " + nMovimenti
+                + " - Massimo movimenti: " + maxMovimenti + " - Massimo prelievo possibile: " + maxPrelievo);
+    }
+
+    void preleva(double x) throws BancaException {
+        if (x > maxPrelievo) {
+            throw new BancaException("il prelievo non è disponibile");
+        }
+        super.preleva(x);
+    }
+}
